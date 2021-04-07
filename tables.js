5754c604279e081da4728a9909d9255587118a29
naeemkhalid1 <email.id>
2021-04-07 17:18:16 +1000

    latest code

--- tables.js
+++ tables.js
@@ -2,42 +2,67 @@ import React, { Component, useEffect, useState } from "react";
 import StyleSheetMethods from "./Styles/StyleSheet";
 import axios from "axios";
 
+// import { StyleSheet, View } from "react-native";
+// import { Table, Row, Rows } from "react-native-table-component";
+
 import {
   Image,
   StyleSheet,
   Text,
   View,
   TouchableOpacity,
+  FlatList,
   Button,
   SafeAreaView,
 } from "react-native";
 import logo from "./assets/boy.png";
 import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
+import { log } from "react-native-reanimated";
 const table = ({ item, img, onDeclineLeave, onReset }) => {
-  function getDbData() {}
+  const [dataHolder, setData] = useState("");
+  const [tableHead, setTableHead] = useState([
+    "Head",
+    "Head2",
+    "Head3",
+    "Head4",
+  ]);
+  // const [isLoading, setLoading] = useState(true);
   useEffect(() => {
-    // getDbData();
-    // fetch("http://localhost:3000/detail/user")
-    //   .then((response) => response.json())
-    //   .then((data) => console.log(data));
-    // let url = "http://localhost:3000/detail";
-    // let response = axios({
-    //   method: "GET",
-    //   url,
-    //   // data: JSON.stringify(data),
-    //   headers: { "content-type": "application/json" },
-    //   // data: data,
-    // });
-    // console.log("response  post= ", response);
     fetch("http://localhost:3000/detail ")
       .then((response) => response.json())
-      .then((data) => console.log("yourData:", data));
-  });
+      .then((data) => {
+        console.log("data", data);
+        data.map((item) => {
+          setData({ dataHolder: item });
+        });
+
+        // setData({ dataHolder: data });
+      });
+  }, []);
 
   return (
     <SafeAreaView style={{ marginTop: 20 }}>
       <View style={StyleSheetMethods.text} key={item}>
-        <Text style={StyleSheetMethods.viewsText}>{item}</Text>
+        <Text>
+          {Object.values(dataHolder).map((item) => {
+            console.log(item);
+            return (
+              <View
+                style={{ flex: 1, flexDirection: "row", marginBottom: 3 }}
+                key={item.id}
+              >
+                <Text style={{ marginRight: 10 }}>{item.name}</Text>
+                <Text style={{ marginRight: 10 }}>{item.phone}</Text>
+                <Text style={{ marginRight: 10 }}>{item.disease}</Text>
+                <Text style={{ marginRight: 10 }}>{item.age}</Text>
+                <Text style={{ marginRight: 10 }}>{item.gender}</Text>
+              </View>
+            );
+          })}
+        </Text>
+        {/* <Text style={StyleSheetMethods.viewsText}>
+          {Object.values(dataHolder).map((item) => {})}
+        </Text> */}
       </View>
 
       <View
@@ -62,4 +87,5 @@ const table = ({ item, img, onDeclineLeave, onReset }) => {
     </SafeAreaView>
   );
 };
+const showData = ({ receiveItems }) => {};
 export default table;
